d6d66b64c0c91b39bbff37114386d0984eb16433
justin.h <hoysoft@qq.com>
2019-12-17 00:15:43 +0800

    add 前后缀

--- db.go
+++ db.go
@@ -16,10 +16,14 @@ type Config struct {
 	DataBase string `default:"cloutAPI"`
 	Encoding string `default:"utf8"`
 	User     string `default:"root"`
-	Password string `required:"true" env:"DBPassword"`
+	Password string `required:"true"`
+	TablePrefix string  //数据表前缀
+	TableSuffix string  //数据表后缀
 }
 
+
 var (
+	//多数据库支持，根据别名定位数据库连接
 	dbList = make(map[string]*gorm.DB)
 )
 
@@ -30,24 +34,30 @@ func New(cnf Config, debug bool, models ...interface{}) *gorm.DB {
 	case "sqlite3":
 		jdb, err = gorm.Open("sqlite3", cnf.DataBase+".db")
 	case "mysql":
-		jdb, err = gorm.Open("mysql", fmt.Sprint("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local", cnf.User, cnf.Password, cnf.Host, cnf.Port, cnf.DataBase, cnf.Encoding))
+		jdb, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local", cnf.User, cnf.Password, cnf.Host, cnf.Port, cnf.DataBase, cnf.Encoding))
 	}
 	if err != nil {
-		panic("连接数据库失败")
+		panic("连接数据库失败:"+err.Error())
 	}
 	if debug {
 		jdb.Debug()
 	}
+
+	// 创建表时添加表前缀/后缀
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return cnf.TablePrefix + defaultTableName + cnf.TableSuffix
+	} 
+
 	if len(models) > 0 {
 		// 自动迁移模式
-		jdb.AutoMigrate(models)
+		jdb.AutoMigrate(models...)
 	}
 
 	dbList[cnf.Alias] = jdb
 	return jdb
 }
 
-//根据别名获取数据库连接
+//根据别名获取数据库连接,不使用别名默认为第一个连接
 func DB(alias ...string) *gorm.DB {
 	if len(alias) > 0 {
 		return dbList[alias[0]]
@@ -62,4 +72,5 @@ func CloseAll() {
 			v.Close()
 		}
 	}
+	println("gormExt is closed")
 }
